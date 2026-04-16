6906ac1c40d2ecee25c6b2523c6ff6a71c72dc67
ckt <cacao.chen@heavengifts.com>
2026-04-16 18:08:14 +0800

    feat: 优化

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
 import router from '@/router/index' // 导入路由配置
 import App from './App.vue'
 let app = createApp(App)
 
 // 定义全局方法
-app.config.globalProperties.$showToast = (message) => {
+app.config.globalProperties.$showToast = (message:any) => {
   alert(`提示：${message}`)
 }
 
 // 使用provide 全局注入方法
-app.provide('$sayHello', (name) => {
+app.provide('$sayHello', (name:any) => {
   alert(`hello：${name}`)
 })
 
